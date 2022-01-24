15af51d6c24ea1b2fbc2d6f86a656b6af29ee155
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-01-23 17:46:35 -0800

    delete unnecesary _document.js and add React Context

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
+import { AppWrapper } from "../context"
 import Layout from "../components/layout"
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AppWrapper>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AppWrapper>
   )
 }
 
