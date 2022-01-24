77307344180b57a56bb2183d165513f5dba608ea
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-01-23 22:39:21 -0800

    add a basic template page with cards

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,8 @@
-export default function Home({ BACKEND_URL }) {
+export default function Home() {
   return (
     <h1 className="container mx-auto text-3xl font-bold">
       Hi Peter Nguyen
       <button className="btn btn-primary rounded-full">daisyUI Button</button>
     </h1>
   )
-}
-
-export async function getStaticProps(context) {
-  const BACKEND_URL = process.env.BACKEND_URL
-
-  return {
-    props: { BACKEND_URL },
-  }
 }
--- pages/template.js
+++ pages/template.js
@@ -1,26 +1,51 @@
-import { useEffect, useState } from "react"
 import axios from "axios"
+import useSWR from "swr"
+
+const fetcher = async (url) =>
+  await axios
+    .get(url)
+    .then((res) => res.data)
+    .catch((err) => {
+      console.error(err)
+      throw new Error(err)
+    })
 
 export default function Template({ BACKEND_URL }) {
-  const [template, setTemplate] = useState({})
+  const { data, error } = useSWR(
+    `${BACKEND_URL}/template/get-all-template`,
+    fetcher
+  )
 
-  useEffect(() => {
-    ;(async function () {
-      const _template = await axios
-        .get(`${BACKEND_URL}/template/get-all-template`)
-        .catch((err) => {
-          console.error(err)
-          isFetchSuccessful = false
-        })
-        .then((res) => res.data)
-      setTemplate(_template)
-    })()
-  }, [])
-
+  if (error)
+    return (
+      <>
+        Error in getting data. Contact us at{" "}
+        <a href="mailto:peter@withdeck.com">peter@withdeck.com</a> and we will
+        resolve this issue as soon as possible.
+      </>
+    )
+  if (!data) return <div>Loading...</div>
   return (
     <div id="template">
-      <p>Template</p>
-      <p>{JSON.stringify(template)}</p>
+      <p>Template length: {data["template"].length}</p>
+      {data["template"].map((template, key) => TemplateCard({ template, key }))}
+    </div>
+  )
+}
+
+function TemplateCard({ template, key }) {
+  const { name, member, app } = template
+
+  return (
+    <div
+      className="card card-bordered card-compact w-fit mt-2 mr-2 bg-white"
+      key={key}
+    >
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p>Number of member: {member.length}</p>
+        <p>Number of app: {Object.keys(app).length}</p>
+      </div>
     </div>
   )
 }
