dbfb0e90d11f4a38297f52ce8bac5d509552d3ab
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-01-26 22:11:31 -0800

    Add app logos

--- pages/template/[id].js
+++ pages/template/[id].js
@@ -25,39 +25,47 @@ export default function Template({ id, BACKEND_URL }) {
 
   if (!data) return <div>Loading...</div>
 
+  // fetch all apps and template's members list from the data received from backend
   const {
     app: { slack, google, atlassian } = {
       slack: undefined,
       google: undefined,
       atlassian: undefined,
     },
+    member,
   } = data
+
+  // filter out missing apps
+  const appsData = [
+    { appData: slack, name: "Slack", imgSrc: Slack_Mark, imgAlt: "Slack" },
+    {
+      appData: google,
+      name: "Google Group",
+      imgSrc: Google_Group,
+      imgAlt: "Google Group",
+    },
+    {
+      appData: atlassian,
+      name: "Atlassian Cloud",
+      imgSrc: Atlassian,
+      imgAlt: "Atlassian",
+    },
+  ].filter((app) => app.appData !== undefined)
 
   return (
     <>
       <h1 className="text-2xl font-bold">{data.name}</h1>
       <hr className="h-0.5 w-1/4 bg-gray-400" />
-      <div>
-        <h2>Applications</h2>
-        {slack && (
-          <Image src={Slack_Mark} height={100} width={100} alt="Slack logo" />
-        )}
-        {google && (
-          <Image
-            src={Google_Group}
-            height={100}
-            width={100}
-            alt="Google Group logo"
-          />
-        )}
-        {atlassian && (
-          <Image
-            src={Atlassian}
-            height={100}
-            width={200}
-            alt="Atlassian Cloud logo"
-          />
-        )}
+      <div className="my-8">
+        <h2 className="text-xl mb-2">Applications ({appsData.length})</h2>
+        <div className="flex flex-row space-x-8">
+          {appsData
+            .filter((app) => Boolean(app.appData))
+            .map((app) => AppCard(app))}
+        </div>
+      </div>
+      <div className="">
+        <h2 className="text-xl mb-2">Members ({member.length})</h2>
       </div>
     </>
   )
@@ -71,6 +79,20 @@ const fetcher = async (url) =>
       console.error(err)
       throw new Error(err)
     })
+
+const AppCard = ({ imgSrc = "", imgAlt = "", name = "" }) => {
+  return (
+    <a
+      href="#"
+      className="p-2 border shadow relative rounded-lg hover:bg-gray-200"
+    >
+      <div>
+        <Image src={imgSrc} height={100} width={200} alt={imgAlt} />
+      </div>
+      <p className="w-full text-center">{name}</p>
+    </a>
+  )
+}
 
 export async function getServerSideProps(context) {
   // Fetch id as the slug of the page from the context.params
