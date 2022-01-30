f10e0ac4441d3e26e63b204f0280265c2721931c
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-01-29 21:33:13 -0800

    add a sidebar skeleton

--- pages/template/[id]/slack.js
+++ pages/template/[id]/slack.js
@@ -1,23 +1,25 @@
+import { Transition } from "@headlessui/react"
+import { useState } from "react"
+
 export default function Slack() {
+  const [isShowing, setIsShowing] = useState(false)
+
   return (
-    <div className="rounded-lg shadow bg-base-200 drawer drawer-end h-52">
-      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-      <div className="flex flex-col items-center justify-center drawer-content">
-        <label htmlFor="my-drawer" className="btn btn-primary drawer-button">
-          open menu
-        </label>
-      </div>
-      <div className="drawer-side">
-        <label htmlFor="my-drawer" className="drawer-overlay"></label>
-        <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-          <li>
-            <a>Menu Item</a>
-          </li>
-          <li>
-            <a>Menu Item</a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <>
+      <button onClick={() => setIsShowing((isShowing) => !isShowing)}>
+        Toggle
+      </button>
+      <Transition
+        show={isShowing}
+        enter="transition-opacity duration-75"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-150"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        I will fade in and out
+      </Transition>
+    </>
   )
 }
