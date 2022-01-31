910557a9a51628de73a864536df21757c0656a46
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-01-31 05:32:33 -0800

    add slack channels update handler

--- components/slack/index.js
+++ components/slack/index.js
@@ -16,7 +16,7 @@ function equalsIgnoreOrder(a = [], b = []) {
 }
 
 // This function renders the sidebar for the Slack app with channels search view and list of channels in the template.
-export function SlackSideBar({ isOpen, setOpen, allConversations, templateConversations }) {
+export function SlackSideBar({ isOpen, setOpen, allConversations, templateConversations, handleSlackChannelsUpdate }) {
   const [addedChannels, setAddedChannels] = useState(templateConversations)
 
   // if the added channels are the same as the template channels from the database,
@@ -29,7 +29,7 @@ export function SlackSideBar({ isOpen, setOpen, allConversations, templateConver
 
   return (
     <div className="w-full h-full divide-y divide-gray-300 space-y-4 p-3">
-      <section className="w-full max-h-1/2 flex flex-col">
+      <section className="w-full h-1/2 flex flex-col">
         <input placeholder="Search channels" className="card w-full mb-4" style={{ padding: "0.5rem" }} />
         <ul className="search-result flex flex-col space-y-2 max-h-80 overflow-y-auto">
           {allConversations.map((conversation, key) => {
@@ -52,7 +52,7 @@ export function SlackSideBar({ isOpen, setOpen, allConversations, templateConver
           })}
         </ul>
       </section>
-      <section className="w-full flex flex-col pt-2 max-h-1/2">
+      <section className="w-full flex flex-col pt-2 h-1/2">
         <h2 className="badge p-1 mt-4 mb-2 w-fit bg-blue-400 text-white">ADDED CHANNELS</h2>
         <ul className="space-y-2 divide-y divide-neutral-300 h-full overflow-y-auto">
           {addedChannels.map((conversation, key) => (
@@ -66,6 +66,10 @@ export function SlackSideBar({ isOpen, setOpen, allConversations, templateConver
           <button
             className={`${shouldSaveActive ? "rounded-btn" : "rounded-btn-disabled"} text-white bg-indigo-500`}
             disabled={!shouldSaveActive}
+            onClick={async (event) => {
+              event.preventDefault
+              await handleSlackChannelsUpdate(addedChannels)
+            }}
           >
             Save
           </button>
