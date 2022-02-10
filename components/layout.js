bd6831bed270b2efe291ddccb61b739170204821
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-02-09 22:25:12 -0800

    fix the login error on auth0

--- components/layout.js
+++ components/layout.js
@@ -1,43 +1,26 @@
 import Menu from "./menu.js"
 import Header from "./header.js"
 import Head from "next/head"
-import { useUser } from "@auth0/nextjs-auth0"
-import Router from "next/router"
-import Spinner from "./spinner"
-import { useEffect } from "react"
+import { withPageAuthRequired } from "@auth0/nextjs-auth0"
 
-export default function Layout(props) {
-  const { user, error, isLoading } = useUser()
-
-  useEffect(() => {
-    if (!user) Router.push("/api/auth/login")
-  })
-
-  if (isLoading)
-    return (
-      <div className="w-full h-full flex justify-center items-center">
-        <Spinner />
-      </div>
-    )
-  if (error) return <div>error.message</div>
-
+function Layout(props) {
   return (
-    user && (
-      <div className="flex flex-row min-h-screen min-w-screen text-black" style={{ backgroundColor: "rgb(241, 245, 249)" }}>
-        <Head>
-          <title>Deck</title>
-          <meta name="title" content="Deck Admin Dashboard" />
-          <meta
-            name="description"
-            content="Deck Admin Dashboard for https://withdeck.com- an application to help you invite teammates into multiple cloud applications in one click"
-          />
-        </Head>
-        <Menu />
-        <div className="w-full h-screen flex flex-col">
-          <Header />
-          <section className="w-full flex-auto">{props.children}</section>
-        </div>
+    <div className="flex flex-row min-h-screen min-w-screen text-black" style={{ backgroundColor: "rgb(241, 245, 249)" }}>
+      <Head>
+        <title>Deck</title>
+        <meta name="title" content="Deck Admin Dashboard" />
+        <meta
+          name="description"
+          content="Deck Admin Dashboard for https://withdeck.com- an application to help you invite teammates into multiple cloud applications in one click"
+        />
+      </Head>
+      <Menu />
+      <div className="w-full h-screen flex flex-col">
+        <Header />
+        <section className="w-full flex-auto">{props.children}</section>
       </div>
-    )
+    </div>
   )
 }
+
+export default withPageAuthRequired(Layout)
