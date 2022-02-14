10ff1796e0d3eecb4e2e59375f2439d95b60667e
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-02-14 11:47:34 -0800

    add hover effect on the close btn for user cards

--- components/template/memberCard.js
+++ components/template/memberCard.js
@@ -9,7 +9,13 @@ export default function MemberCard({ email, name, _id, key, removeUser }) {
       className="p-2 border shadow relative rounded-lg hover:bg-gray-200 flex flex-col relative"
       title={name}
     >
-      <XCircleIcon className="absolute top-0 right-0 m-2 w-5 h-5" onClick={async () => await removeUser(_id)} />
+      <XCircleIcon
+        className="absolute top-0 right-0 m-2 w-5 h-5 hover:text-red-600"
+        onClick={async (event) => {
+          event.preventDefault()
+          await removeUser(_id)
+        }}
+      />
       <div>
         <UserIcon style={{ height: 100, width: 200 }} />
       </div>
